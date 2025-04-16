10155a47b319d2a9e2f163ecb45fda72c90e9288
qinzl <qinzl@jxgtmbjt.com>
2025-04-16 10:09:06 +0800

    init proj

---
+++ hardhat.config.js
@@ -0,0 +1,37 @@
+require("@nomicfoundation/hardhat-toolbox");
+// require("@chainlink/env-enc").config()
+require("dotenv").config()
+require("hardhat-deploy");
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const PRIVATE_KEY_1 = process.env.PRIVATE_KEY_1
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY, PRIVATE_KEY_1],
+      chainId: 11155111
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY
+    }
+  },
+  namedAccounts: {
+    firstAccount: {
+      default: 0
+    },
+    secondAccount: {
+      default: 1
+    },
+  },
+  sourcify: {
+    enabled: true
+  },
+};
